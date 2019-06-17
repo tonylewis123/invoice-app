e57dec6c929baa58820617c6eacdfc9bb1a3f597
tonylewis123 <tony@webmasterfreelanceonlinedev.site>
2019-06-17 12:13:44 +0400

    Test commit

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ const api = require('./controllers/Api');
 api.initializeApp(app);
 
 if (process.env.NODE_ENV === 'production') {
+  console.log('===============');
   app.use(express.static(path.join(__dirname, 'public')));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
