1df65fd7279c2ed6266be85d63db5afc18211fc6
ArmenKarapetyan1992 <50479349+ArmenKarapetyan1992@users.noreply.github.com>
2019-06-19 18:46:41 +0400

    super admin page

---
+++ client/src/components/project/save_btn/Save_btn.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './save_btn.css';
+
+export default function Save_btn (props){
+        return(
+            <div className="next_btn_div">
+            <button className="next_btn">save </button>
+            </div>
+        )
+}
--- client/src/components/project/super_admin_page/Super_admin_page.js
+++ client/src/components/project/super_admin_page/Super_admin_page.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './super_admin_page.css';
 import Header from '../header/Header';
 import background from "../../../assets/img/login_bg.png";
-import { POST } from "../../../core/CRUD"
+import { POST, GET, DELETE } from "../../../core/CRUD"
+import { async } from 'q';
 
 export default class Super_admin_page extends React.Component {
     constructor(props) {
         super(props);
     }
     state = {
-        inputs:{
+        inputs: {
             email: {
                 value: '',
                 isTuched: false,
@@ -26,11 +27,18 @@ export default class Super_admin_page extends React.Component {
                 value: "employer"
             },
         },
-        users:[],
-        issetUsers : false
-       
+        users: [],
+        error: ""
     }
-        
+
+   async componentDidMount (){
+        let res = await  GET('api/admin/usersList');
+        if(!res.success){
+           return this.setState({error: res.error}); 
+        } 
+        this.setState({users: res.data});   
+    }
+
     changeInputHandle = (element, value) => {
         if (element === 'email') {
             let email = this.state.inputs.email;
@@ -39,22 +47,19 @@ export default class Super_admin_page extends React.Component {
             if (re.test(value)) {
                 email.isTuched = true;
                 email.isValid = true
-            }
-            else {
+            } else {
                 email.isTuched = true;
                 email.isValid = false
             }
             this.setState({ email })
 
-        }
-        else if (element === 'name') {
+        } else if (element === 'name') {
             let name = this.state.inputs.name;
             name.value = value;
             if (value === '' || value === null) {
                 name.isTuched = true;
                 name.isValid = false
-            }
-            else {
+            } else {
                 name.isTuched = true;
                 name.isValid = true
             }
@@ -68,26 +73,45 @@ export default class Super_admin_page extends React.Component {
         role.value = name;
         this.setState({ role })
     }
-    createUser = async (data) => {
+    createUser = async(data) => {
         let { email, name, role } = data;
         if (!email.value || !name.value || !role.value) {
             return this.setState({ error: 'All fields are required!' });
         }
-        else{
-            const users = this.state.users;
-            users.push(this.state.inputs);
-            this.setState({users,issetUsers:true})
+
+        let response = await POST('api/admin/create_user', {
+            email: email.value,
+            role: role.value,
+            fullName: name.value
+        });
+        if(!response.success){
+            return this.setState({error: response.error});
         }
-            
-        
-        // let response = await POST('api/admin/create_user', {
-        //     email: email.value,
-        //     role: role.value,
-        //     fullName: name.value
-        // });
-        // console.log(response, '===============');
+        this.setState({users: response.data, error: '', });
     }
-  
+
+    deleteUser = async (userId) => {
+        let res = await DELETE('api/admin/delete_user', { userId });
+        if(!res.success){
+            return this.setState({error: res.error});
+        }
+        this.setState({
+            users: res.data,
+            error: ''
+        });
+    }
+
+    generateListItem = data => {
+        return data.map((user, index) => {
+            return(
+                <tr key = { index } className="user_list">
+                  <td>{index + 1}</td>  <td> { user.email }</td> <td> {user.fullName} </td> <td>{ user.role }</td> <td> <button onClick={() => this.deleteUser(user._id)}
+                    >delete</button> </td>
+                </tr>
+            )
+        })
+    }
+
     render() {
         let background_page = {
             backgroundImage: `url(${background})`,
@@ -96,36 +120,47 @@ export default class Super_admin_page extends React.Component {
             <div className="Admin_page" style={background_page}>
                 <div className="admin_page_size">
                     <Header />
-                    <div className="add_new_project_btn">
-                        <button onClick={async () => this.createUser(this.state.inputs)}>Create</button>
+                    {this.state.error? <div>{this.state.error}</div> : null}
+                    <div className="sup_admin_clear"></div>
+                    <div className="sup_admin_page_search">
+                        <p>{this.state.inputs.email.isTuched && !this.state.inputs.email.isValid ? this.state.inputs.email.massage : ''}</p>
+                        <input type="text" placeholder="Email" value={this.state.inputs.email.value} onChange={event => this.changeInputHandle('email', event.currentTarget.value)} />
+                        <p> {this.state.inputs.name.isTuched && !this.state.inputs.name.isValid ? this.state.inputs.name.massage : ''} </p>
+                        <input type="text" placeholder="Full name" value={this.state.inputs.name.value} onChange={event => this.changeInputHandle('name', event.currentTarget.value)} />
                     </div>
                     <div className="admin_page_inputs">
                         <div className="admin_page_inputs_filtr">
-                            <input onChange={this.radioInputChange.bind(this, 'employer')} type="radio" name="user"  value='employer' />
+                            <input type="radio" name="user" checked value = 'employer' onChange = { this.radioInputChange.bind(this, 'employer') }/>
                             <p>Employee</p>
                         </div>
                         <div className="admin_page_inputs_filtr admin_page_inputs_filtr_2">
-                            <input onChange={this.radioInputChange.bind(this, 'admin')} type="radio" name="user" value='admin' />
+                            <input type="radio" name="user" value = 'admin' onChange = { this.radioInputChange.bind(this, 'admin') } />
                             <p>Admin</p>
                         </div>
                     </div>
-                    <div className="admin_page_search">
-                        <p>{this.state.inputs.email.isTuched && !this.state.inputs.email.isValid ? this.state.inputs.email.massage : ''}</p>
-                        <input type="text" placeholder="Email" value={this.state.inputs.email.value} onChange={event => this.changeInputHandle('email', event.target.value)} />
-                        <p>{this.state.inputs.name.isTuched && !this.state.inputs.name.isValid ? this.state.inputs.name.massage : ''}</p>
-                        <input type="text" placeholder="Full name" value={this.state.inputs.name.value} onChange={event => this.changeInputHandle('name', event.target.value)} />
+                    <div className="add_new_project_btn">
+                        <button onClick={async () => this.createUser(this.state.inputs)} >Create</button>
                     </div>
-                    <ul></ul>
-                   {this.state.issetUsers?this.state.users.map((elem,index)=>{
-                     
-                        return(
-                            <li key={index}> <span>{elem.email.value}</span> <span>{elem.name.value}</span> <span>{elem.role.value}</span> </li>
-                        )
-                   }):''}
-                
-
+                    <div  className="user_table_content">
+                    <table className="users_list">
+                        <thead>
+                            <tr>
+                                <th>N</th>
+                                <th>Email</th>
+                                <th>Fullname</th>
+                                <th>Role</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {this.state.users.length > 0 ? this.generateListItem(this.state.users) : "" }
+                        </tbody>
+                    </table>
+                    </div>
+                    <div className="sup_admin_clear"></div>
                 </div>
             </div>
         )
     }
+    
 }
